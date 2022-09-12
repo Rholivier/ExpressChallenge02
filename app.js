e55135dd05c822f747601b116a232f671f45cb24
Rholivier <olivier.terral@sncf.fr>
2022-09-12 16:08:17 +0200

    Challenge 8 Bonus not done

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 
-const { hashPassword } = require("./auth.js");
-
 const app = express();
 
 app.use(express.json());
@@ -17,18 +15,27 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 
 const userHandlers = require("./userHandlers");
 
+// PUBLICS ROUTES
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+// AUTHENTIFICATION WALL AND PROTECTED ROUTES
+app.use(verifyToken);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 
 // déterminer les options de hachage
 const hashingOptions = {
@@ -24,4 +25,51 @@ const hashPassword = (req, res, next) => {
     });
 };
 
-module.exports = { hashPassword };
+const verifyPassword = (req, res) => {
+  argon2
+    .verify(req.user.hashedPassword, req.body.password)
+    .then((isVerified) => {
+      if (isVerified) {
+        // payload = informations du user encodées, sub = sujet (ie le user)
+        const payload = { sub: req.user.id };
+        // le jeton doit être signé avec une chaine secrète ("JWT_SECRET") stockée dans .env
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        // on supprime le password saisie et on envoie le token au user
+        delete req.user.hashedPassword;
+        res.send({ token, user: req.user });
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { hashPassword, verifyPassword, verifyToken };
